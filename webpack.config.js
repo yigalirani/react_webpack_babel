22455bda1490bc5afee3e4219e4f9485d34a7e4a
Yigal Irani <yigal@symbolclick.com>
2019-07-07 16:08:41 -0400

    Make webpack download react from cdn as described https://stackoverflow.com/questions/56917831/make-webpack-download-react-from-cdn

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
+   externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
   module: {
     rules: [
       {
